36db851ec49fd3239ccd9f7deb692856503f9f8d
sergey.antonov <sergey.antonov@nedra.digital>
2023-01-20 07:40:04 +0300

    feat(событие) * добавил визуализацию карточки события

--- src/pages/Events/elems/EventInfo/EventInfoMediaDisplay.tsx
+++ src/pages/Events/elems/EventInfo/EventInfoMediaDisplay.tsx
@@ -1,4 +1,5 @@
 import { Box, Card, CardMedia, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import React from 'react';
 import { useSelector } from 'react-redux';
 
@@ -6,17 +7,84 @@ import { selectEventStateImageData } from '../../../../domen/events/eventsSelect
 import { HTTP_URL } from '../../../../typings/env';
 import { isEqual } from '../../../../utils/functions/isEqual';
 
+const StyledCard = styled(Card)(
+  () => `
+    width: 470px;
+    height: 396px;
+    position: relative;
+    box-shadow: none;
+    border-radius: 28px;
+`,
+);
+
+const StyledCardMedia = styled(CardMedia)(
+  () => `
+    height: 330px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    &:after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(0deg, #111633 19%, rgba(0, 0, 3, 0.09) 32%);
+    }
+`,
+);
+
+const StyledBox = styled(Box)(
+  () => `
+    position: absolute;
+    bottom: 1rem;
+    left: 1rem;
+`,
+);
+
+const StyledTypography = styled(Typography)(
+  () => `
+    font-weight: 400;
+    font-size: 28px;
+    line-height: 38px;
+    color: #C7C7DE;
+`,
+);
+
+const StyledTypographyDate = styled(Typography)(
+  () => `
+    position: absolute;
+    bottom: 7rem;
+    left: 1rem;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 28px;
+    line-height: 31px;
+    color: #00E3B5;
+    text-shadow: 0px 4px 14px rgba(0, 227, 181, 0.69);
+    &:after {
+      content: '';
+      position: absolute;
+      top: 0.3rem;
+      left: -1rem;
+      width: 9px;
+      height: 22px;
+      background: #00E3B5;
+    }
+`,
+);
+
 export const EventInfoMediaDisplay = () => {
   const { image, title } = useSelector(selectEventStateImageData, isEqual);
 
   console.log('render EventInfoMediaDisplay');
 
   return (
-    <Card sx={{ width: '100%', height: 298, display: 'inline-block', position: 'relative' }}>
-      {image && <CardMedia sx={{ height: 298 }} image={`${HTTP_URL}${image.path}`} title={image.name} />}
-      <Box sx={{ position: 'absolute', bottom: '1rem', left: '1rem' }}>
-        <Typography variant='h3'>{title}</Typography>
-      </Box>
-    </Card>
+    <StyledCard>
+      {image && <StyledCardMedia image={`${HTTP_URL}${image.path}`} title={image.name} />}
+      <StyledTypographyDate>25 янв</StyledTypographyDate>
+      <StyledBox>
+        <StyledTypography>{title}</StyledTypography>
+      </StyledBox>
+    </StyledCard>
   );
 };

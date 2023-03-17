84973d02071294b5cad3f8e0dd9692e42b33926b
sergey.antonov <sergey.antonov@nedra.digital>
2023-03-17 15:06:33 +0300

    fix(media) * fix get image src

--- src/domens/medialibrary/helpers/getImageSrc.ts
+++ src/domens/medialibrary/helpers/getImageSrc.ts
@@ -1,6 +1,6 @@
 import { HTTP_URL } from '../../../typings/env';
 import { ImageSizes } from '../types/types';
 
-export const getImageSrc = (paths: string[], size: ImageSizes) => {
-  return `${HTTP_URL}${paths.find((path) => path.includes(size))}`;
+export const getImageSrc = (paths: string[] | undefined, size: ImageSizes) => {
+  return `${HTTP_URL}${paths?.find((path) => path.includes(size))}`;
 };

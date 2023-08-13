57d79580eaa079e9f12b02068ea29eef6c0788ed
sergei_antonov <jdvarmy@gmail.com>
2023-08-13 12:34:05 +0300

    feat(taxonomy): add column

--- src/domens/medialibrary/types/types.ts
+++ src/domens/medialibrary/types/types.ts
@@ -4,9 +4,9 @@ export const mapScope = 'map' as const;
 
 export enum ImageSizes {
   origin = 'origin',
-  xs = '200x278',
+  // xs = '200x278',
   s = '430x366',
   m = '660x470',
-  l = '1340x560',
-  xl = '1920x1080',
+  // l = '1340x560',
+  // xl = '1920x1080',
 }
